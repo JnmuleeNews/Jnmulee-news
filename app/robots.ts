40dcce5b9906ebac6c9f82ab84d00629013a7f2d
JnmuleeNews <jnquickpay@gmail.com>
2026-09-23 08:08:12 -0400

    Create robots.ts

---
+++ app/robots.ts
@@ -0,0 +1,19 @@
+import { MetadataRoute } from "next";
+
+export default function robots(): MetadataRoute.Robots {
+  const baseUrl =
+    process.env.NEXT_PUBLIC_SITE_URL ||
+    "https://jnmulee-news.vercel.app";
+
+  return {
+    rules: {
+      userAgent: "*",
+      allow: "/",
+      disallow: [
+        "/admin/",
+        "/api/",
+      ],
+    },
+    sitemap: `${baseUrl}/sitemap.xml`,
+  };
+}
